4a68a7b5a0f03268be488392c09bd6586587a1d3
Taeyoung Son <taeyoung2.son@samsung.com>
2015-01-14 15:57:14 +0900

    Added ant script Modified ant call
    Change-Id: I9f83d38ffe4ac4467fb8ea4dd49751816df3b1e6
    Signed-off-by: Taeyoung Son <taeyoung2.son@samsung.com>

--- src/main/java/com/tysonista/tizencommitlog/TizenGitParameterDefinition.java
+++ src/main/java/com/tysonista/tizencommitlog/TizenGitParameterDefinition.java
@@ -3,8 +3,13 @@ package com.tysonista.tizencommitlog;
 import hudson.Extension;
 import hudson.model.ParameterValue;
 import hudson.model.ParameterDefinition;
+import hudson.util.ArgumentListBuilder;
 import hudson.util.FormValidation;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -83,10 +88,68 @@ public class TizenGitParameterDefinition extends ParameterDefinition {
                 @QueryParameter("port") String port,
                 @QueryParameter("path") String path,
                 @QueryParameter("branch") String branch) {
-            return FormValidation.ok("gitUrl:"+url+
-                    ", gitPort:"+port+
-                    ", gitPath:"+path+
-                    ", gitBranch:"+branch);
+            try {
+                return antCall(url, port, path, branch);
+            } catch (IOException e) {
+                return FormValidation.error(e, e.getLocalizedMessage());
+            } catch (InterruptedException e) {
+                return FormValidation.error(e, e.getLocalizedMessage());
+            }
+//            return FormValidation.ok("gitUrl:"+url+
+//                    ", gitPort:"+port+
+//                    ", gitPath:"+path+
+//                    ", gitBranch:"+branch);
+        }
+        public FormValidation antCall(String url, String port, String path, String branch) throws IOException, InterruptedException {
+            // This also shows how you can consult the global configuration of the builder
+            String buildXmlDirPath = "/home/wac/jenkins/plugins/tizen-commit-log/";
+            String buildXmlFilePath = buildXmlDirPath+"ant-version.xml";
+
+            ArgumentListBuilder args = new ArgumentListBuilder();
+            args.add("ant");
+            if (buildXmlFilePath!=null) {
+                args.add("-file", buildXmlFilePath);
+            }
+
+            StringBuilder properties = new StringBuilder();
+            properties.append("GIT_SERVER_URL=").append(url).append("\n")
+                                                .append("GIT_SERVER_PORT=").append(port).append("\n")
+                                                .append("GIT_PROJECT_NAME=").append(path).append("\n")
+                                                .append("GIT_BRANCH_NAME=").append(branch).append("\n");
+            args.addKeyValuePairsFromPropertyString("-D", properties.toString(), null, null);
+
+            List<String> newArgs = new ArrayList<String>(args.toList());
+            newArgs.set(newArgs.size() - 1, newArgs.get(newArgs.size() - 1).replaceAll("(?<= )(-D[^\" ]+)= ", "$1=\"\" "));
+            args = new ArgumentListBuilder(newArgs.toArray(new String[newArgs.size()]));
+            args.add("commit");
+
+            Process process;
+            process = Runtime.getRuntime().exec(args.toList().toArray(new String[0]));
+            int exitValue = process.waitFor();
+            String logs = getString(process.getInputStream());
+            logs = "command:"+args.toString()+"\n"+logs;
+            if (exitValue != 0) {
+                String errs = getString(process.getErrorStream());
+                return FormValidation.error("ant build failed("+ exitValue+")\n"+logs+"\n"+errs);
+            } else {
+                return FormValidation.ok(logs);
+            }
+        }
+        private String getString(InputStream is) throws IOException {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new InputStreamReader(is));
+                StringBuffer sb = new StringBuffer();
+                String line;
+                while ((line = br.readLine())!= null) {
+                    sb.append(line).append("\n");
+                }
+                return sb.toString();
+            } finally {
+                if (br != null) {
+                    br.close();
+                }
+            }
         }
     }
 }
